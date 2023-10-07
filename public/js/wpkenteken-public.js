7f746c9f4f17e32d0eb98a42093f1838ef615137
Sander van der Windt <sandervdwindt@gmail.com>
2023-10-07 17:27:35 -0400

    Added working AJAX script with static API key

--- public/js/wpkenteken-public.js
+++ public/js/wpkenteken-public.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+(function ($) {
 	'use strict';
 
 	/**
@@ -29,4 +29,56 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
-})( jQuery );
+	$(window).load(function () {
+
+		$('.wpkenteken-kenteken').focusout(function () {
+
+			// Validate input.
+
+			const invoer = document.querySelector('.wpkenteken-kenteken > input[type="text"]').value;
+			console.log(invoer);
+			let kenteken = invoer.replace(/[^0-9a-zA-Z]/g, "");
+			kenteken = kenteken.toUpperCase();
+			console.log(kenteken);
+			if (kenteken.length !== 6) {
+				alert('De invoer is ongeldig');
+				return false;
+			}
+
+			// AJAX request.
+
+			$.ajax({
+				url: "https://opendata.rdw.nl/resource/qyrd-w56j.json?kenteken=" + kenteken,
+				type: "GET",
+				dataType: 'json',
+				data: {
+					"$limit": 5000,
+					"$$app_token": "NnFU01CF99CsjkG7hGMIWIqiC"
+				},
+				success: function (data) {
+					console.log('succes');
+					var response = data[0];
+					console.log(response);
+					if (typeof response !== 'undefined') {
+						console.log(response.merk);
+						console.log(response.model);
+						console.log(response.bouwjaar);
+						document.querySelector('.wpkenteken-merk > input[type="text"]').value = response.merk;
+						document.querySelector('.wpkenteken-model > input[type="text"]').value = response.handelsbenaming;
+						document.querySelector('.wpkenteken-bouwjaar > input[type="text"]').value = response.datum_eerste_toelating.substring(0, 4);
+						$('.model').val(response.handelsbenaming);
+						var bouwjaar = (response.datum_eerste_toelating).substring(0, 4);
+						$('.bouwjaar').val(bouwjaar);
+					} else {
+						$('.response').text('Geen resultaat gevonden. Check het ingevoerde kenteken. of probeer het later nog eens.');
+					}
+				},
+				error: function(data) {
+					alert('Error');
+				}
+			
+			});
+		});
+	});
+
+})(jQuery);
